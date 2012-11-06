3caa72f6e54abaa1ac4b5fe10cd330502975c1f1
Sławosz <slawosz@gmail.com>
2012-11-06 23:03:26 +0100

    use ruby file and fileutils methods in list fetcher spec

--- spec/cran_browser_backend/list_fetcher_spec.rb
+++ spec/cran_browser_backend/list_fetcher_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
+require 'fileutils'
 
 describe CranBrowserBackend::ListFetcher do
   let(:url) { CranBrowserBackend::ListFetcher::URL }
 
-  let(:sandbox_path) { CranBrowserBackend::ROOT + '/spec/sandbox' }
+  let(:sandbox_path) { File.expand_path('spec/sandbox', CranBrowserBackend::ROOT) }
 
   before do
     stub_const("CranBrowserBackend::SANDBOX", sandbox_path)
-    `cp #{CranBrowserBackend::ROOT}/spec/fixtures/PACKAGES.gz #{sandbox_path}`
+    file_to_copy = File.expand_path('spec/fixtures/PACKAGES.gz',CranBrowserBackend::ROOT)
+    FileUtils.cp(file_to_copy, sandbox_path)
   end
 
   it 'should download package and get package dcf' do
     CranBrowserBackend::Downloader.should_receive(:get).with(url)
-    Dcf.should_receive(:parse).with(File.read(CranBrowserBackend::ROOT + '/spec/fixtures/PACKAGES'))
+    Dcf.should_receive(:parse).with(File.read(File.expand_path('spec/fixtures/PACKAGES', CranBrowserBackend::ROOT)))
 
     CranBrowserBackend::ListFetcher.fetch
 
