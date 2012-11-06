9990cfba1f9695e5bd6515ce4bfd9d482131d308
Sławosz <slawosz@gmail.com>
2012-11-06 19:36:48 +0100

    refactor: extract code to method

--- lib/cran_browser_backend/packages_fetcher.rb
+++ lib/cran_browser_backend/packages_fetcher.rb
@@ -4,12 +4,20 @@ module CranBrowserBackend
     BASE_URL = "http://cran.r-project.org/src/contrib/"
     FILE_TYPE = "tar.gz"
 
-    def self.fetch(packages_to_fetch)
-      packages_to_fetch.map do |package|
-        package_data = { url: BASE_URL + package["Package"] + '_' + package["Version"] + '.' + FILE_TYPE,
-          file_name: package["Package"] + '_' + package["Version"] + '.' + FILE_TYPE,
-          directory_name: package["Package"]}
-        PackageFetcher.fetch(package_data)
+    class << self
+      def fetch(packages_to_fetch)
+        packages_to_fetch.map do |package|
+          @package = package
+          PackageFetcher.fetch(get_package_data)
+        end
+      end
+
+      private
+
+      def get_package_data
+        { url: BASE_URL + @package["Package"] + '_' + @package["Version"] + '.' + FILE_TYPE,
+            file_name: @package["Package"] + '_' + @package["Version"] + '.' + FILE_TYPE,
+            directory_name: @package["Package"]}
       end
     end
   end
